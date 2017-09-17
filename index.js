5794e16235db6feec91cb376ff99521831a328bc
cab driver <cocktailsthecomic@gmail.com>
2017-09-17 20:53:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function getFirstSelector(test) {
   return document.querySelector(test)}
 function nestedTarget(){
-  var nested = document.queryslector('#nested .target');
+  var nested = document.queryslector('#nested.target');
   return nested
   }
