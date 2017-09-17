4b0de6c9c3ae3a47d1c66613a9818be2a692123c
cab driver <cocktailsthecomic@gmail.com>
2017-09-17 20:49:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
 function getFirstSelector(test) {
   return document.querySelector(test)}
-
-function nestedTarget(){
-     var pop = document.querySelector('#nested .target');
-     return pop}
-
-function increaseRankBy(n){
-        var zzz = document.querySelectorAll('ul.ranked-list li')
-        for (let i = 0; i < zzz.length; i++){
-              zzz[i].innerHTML = parseInt(zzz[i].innerHTML) + n}}
-function deepestChild(){
-  function find(node, critera){
-    
-  }
-}
